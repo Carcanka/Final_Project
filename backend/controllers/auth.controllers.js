c085ce49770fc77d345e971ae10bec507101d641
Carcanka <carmelobarmar@hotmail.com>
2023-08-11 08:26:42 -0300

    tablas completas

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,15 +1,21 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { knex } = require("../db");
-const { body } = require("express-validator");
+// const { body } = require("express-validator");
+
+const sendToken = (res, next, email, nombre) => {
+  const token = jwt.sign({ email, nombre }, secret);
+  res.json({ token });
+  next();
+};
 
 const secret = "encriptado";
 exports.secret = secret;
 exports.register = async (req, res, next) => {
-  // const salt = await bcrypt.genSalt(10);
-  const hashContrasenia = await bcrypt.hash(req.body.password, 10);
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-  await knex("usuarios").insert({ nombre:req.body.nombre, email:req.body.email, password_hash: hashContrasenia });
+  await knex("usuarios").insert({ nombre:req.body.nombre, email:req.body.email, password_hash: hashPassword });
 
   sendToken(res, next, req.body.email, req.body.nombre);
 };
@@ -22,7 +28,7 @@ exports.login = async (req, res, next) => {
   const usuario = respuestaUsuarios[0];
 
   if (!usuario) {
-    res.status(404).json({ mensje: "usuario/password incorrecta" });
+    res.status(404).json({ mensaje: "usuario incorrecto" });
     next();
     return;
   }
@@ -33,16 +39,10 @@ exports.login = async (req, res, next) => {
   );
 
   if (!passwordValida) {
-    res.status(404).json({ mensje: "usuario/password incorrecta" });
+    res.status(404).json({ mensaje: "password incorrecta" });
     next();
     return;
   }
 
   sendToken(res, next, email, usuario.nombre);
-};
-
-const sendToken = (res, next, email, nombre) => {
-  const token = jwt.sign({ email, nombre }, secret);
-  res.json({ token });
-  next();
 };
