bf0cd52f6ffffa762de47dd9a38606e7e10b11d1
¨Alejandro <alejandro.cuadrado85@gmail.com>
2023-08-15 15:23:10 -0300

    login solved

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,32 +1,84 @@
-import React from 'react';
-import './Login.css';
-import { Link } from 'react-router-dom';
-import Flecha from '../../assets/left-icon-placeholder.svg';
+import React, { useEffect, useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import Flecha from "../../assets/left-icon-placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate(); 
+  const token = localStorage.getItem("autorizacion");
+  const [form, setForm] = useState({    
+    email: "",
+    password: "",
+  });
+  const senddata = async (e) => {
+    console.log(form);
+    e.preventDefault();
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      email: form.email,
+      password: form.password,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    const res = await fetch("http://localhost:3000/auth/login", requestOptions);
+    const data = await res.json();
+    localStorage.setItem("token", data.token );
+    console.log(data);
+  };
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setForm({ ...form, [name]: value });
+  };
+
   return (
     <>
       <div className="login-container">
         <div className="header-gradient"></div>
         <div className="login-header">
-        <button><Link to={`/`}><img src={Flecha} alt="Ir atrás" /></Link></button>      
-            
-          
+          <button>
+            <Link to={`/`}>
+              <img src={Flecha} alt="Ir atrás" />
+            </Link>
+          </button>
+
           <h3>Iniciar Sesión</h3>
           <div></div>
         </div>
-        <form action="">
+        <form action="" onSubmit={senddata}>
           <div className="input-container">
-            <h2>Nombre de Usuario o E-mail:</h2>
-            <input type="text" className="login-input" />
+            <h2>Ingrese E-mail:</h2>
+            <input
+              name="email"
+              onChange={onInputChange}
+              type="text"
+              className="login-input"
+            />
           </div>
 
           <div className="input-container">
             <h2>Contraseña:</h2>
-            <input type="password" className="login-input" />
+            <input
+              name="password"
+              onChange={onInputChange}
+              type="password"
+              className="login-input"
+            />
           </div>
 
-          <input type="submit" className="login-submit" value="Iniciar Sesión" />
+          <button type="submit" className="login-submit"><Link to="/home" className="footer-link">
+          Iniciar sesion
+          </Link>            
+          </button>
           <p>
             <a href="" className="reset-password">
               ¿Olvidaste tu contraseña?
