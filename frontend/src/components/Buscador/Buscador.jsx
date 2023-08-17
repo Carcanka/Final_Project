ba7c18aeefb9c69f017200eedd28b7e97147b304
Carcanka <carmelobarmar@hotmail.com>
2023-08-17 11:46:59 -0300

    fotos funcionando

--- frontend/src/components/Buscador/Buscador.jsx
+++ frontend/src/components/Buscador/Buscador.jsx
@@ -48,7 +48,8 @@ function Buscador() {
             {filteredSongs.map((song) => (
               <li className="buscador-card" key={song.id}>
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/d5c7/d566/fc87a0ec2aecfa854c52d3bd5552c631?Expires=1693180800&Signature=k9C6Yhllm8ZPMtGLQkqh1nTs~t3xgaEX~rqNqbVXItfP4AFdR6qTA~2kF7uN5ML3bIgxTx8tM1e0yBhxBaPlzMf7Le68rBmSnBEEKLShS~wO1tPPrG71hOHVVCQhl9fRew~KhimnPbWxz4mji3x7FRkKMbE9jn3TBNZx5KU76~GLfgxqYy4gonbak9VQ78OF2j4av6-xU0zJjcsqBQ5IGm-J5nwBANWaJ2VdKqNoUS6zb-Sn1~amhEyTCBAu3Ubl0ojHIdMRefjxhf3z84DLPnD0REd004sB6LEiKdhhyc5Sm4SSvunEs14-FaKgk75V5Q-WtIouy5O4bQW4ADkHEg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  src={new URL(`../../artistas/${song.artista_id}.jpg`, import.meta.url)}
+                  // style={{background:`url(${ new URL(`../../artistas/${song.artista_id}.jpg`, import.meta.url)})`}}
                   alt="imagen musical"
                 />
                 <h3>{song.titulo}</h3>
